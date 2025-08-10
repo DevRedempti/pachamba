a15e0a09db6e3957c992cd2228ee2dc9319008a2
DevRedempti <dev.redempti@gmail.com>
2025-08-10 20:26:35 +0800

    adding laravel sanctum

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,7 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $sample = array("");
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
